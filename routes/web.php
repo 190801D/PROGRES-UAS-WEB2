0cdb1bdb38efea447d97113b9144c4ffefd0ae8f
Devi Wulandari <deviwulandari200902@gmail.com>
2024-07-11 02:00:26 +0700

    crud

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::resource('/posts', PostController::class);
 
 Route::resource('/posts', PostController::class);
-Route::get('/view', [PostController::class, 'view'])->name('posts.view');
-Route::get('/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::get('/view/{code}', [PostController::class, 'view'])->name('posts.view');
+Route::get('/add', [PostController::class, 'add'])->name('posts.add');
+Route::get('/edit/{code}', [PostController::class, 'edit'])->name('posts.edit');
 Route::get('/login', [PostController::class, 'login'])->name('posts.login');
+Route::get('/pdf', [PostController::class, 'generatePDF'])->name('posts.pdf');
